67b01bc7ed0cff00660002aa93bc3b706244fe37
yukselibrahim <113780430+yukselibrahim@users.noreply.github.com>
2022-10-24 17:37:07 +0300

    C#
    Fill student information with method

---
+++ Student_Information/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Information
+{
+    public class Student
+    {
+        private string studentNum;
+        private string name;
+        private string surname;
+        private double exam1;
+        private double exam2;
+        private double examFinal;
+        private string schoolName;
+
+        public Student(string _studentNum,string _name, string _surname, double _exam1, double _exam2, double _examFinal, string _schoolName)
+
+        {
+            studentNum = _studentNum;
+            name = _name;
+            surname = _surname;
+            exam1 = _exam1;
+            exam2 = _exam2;
+            examFinal = _examFinal;
+            schoolName = _schoolName;
+        }
+
+
+        public  void StudentInfo()
+        {
+            Console.WriteLine("Student Number: "+studentNum);
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Student Surname: " + surname);
+            Console.WriteLine("Student First Exam: " + exam1);
+            Console.WriteLine("Student Second Exam: " + exam2);
+            Console.WriteLine("Student Final Exam: " + examFinal);
+            Console.WriteLine("School Name: " + schoolName);
+        }
+
+
+        public double ExamAverage()
+        {
+            double average = exam1 * 0.2 + exam2 * 0.2 + examFinal * 0.6;
+            return average;
+        }
+
+        public void SchoolName()
+        {
+            Console.WriteLine("School Name: "+schoolName);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Student_Information/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("1","İbrahim","Yüksel",50,55,73,"Jozef Pilsudski University");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("********************Welcome********************");
+            Console.WriteLine();
+
+            bool control = true;
+            Console.ForegroundColor = ConsoleColor.White;
+            while (control)
+            {
+                Console.WriteLine();
+                Options();
+                string select = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (select)
+                {
+                    case "1":
+                      student1.StudentInfo();
+                        break;
+
+                    case "2":
+                        double examAverage = student1.ExamAverage();
+                        Console.WriteLine("Student's Exam Average: "+examAverage);
+                        break;
+
+                    case "3":
+                        student1.SchoolName();
+                        break;
+
+                    case "4":
+                        control = false;
+                        break;
+                        
+                }
+            }
+
+            Console.WriteLine("Have a nice day");
+            Console.ReadLine();
+
+        }
+
+        static void Options()
+        {
+            Console.WriteLine("1- Student's Informations");
+            Console.WriteLine("2- Student's Exam Average");
+            Console.WriteLine("3- Student's School Name");
+            Console.WriteLine("4- Exit");
+        }
+    }
+}
